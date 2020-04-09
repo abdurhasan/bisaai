eb478f7c4c95440f04aaf85b5cf742a2083c7384
abdurhasan <abdurhasan20@gmail.com>
2020-04-09 18:47:40 +0700

    microservices

--- app-persisted/helper.js
+++ app-persisted/helper.js
@@ -1,3 +1,10 @@
+require('dotenv').config()
+
+const { MongoClient } = require('mongodb');
+const mongoUri = process.env.MONGODB_URI
+const mongoDatabase = process.env.MONGODB_DATABASE
+const mongoCollection = process.env.MONGODB_COLLECTION
+
 function IsValidJSONString(str) {
     try {
         JSON.parse(str);
@@ -8,12 +15,73 @@ function IsValidJSONString(str) {
 }
 
 function StringFilter(str) {
-    return str.replace(/(aaaa,?|\n)/g," ")
+    return str.replace(/(aaaa,?|\n)/g, " ")
 }
 
 
 
+
+
+const executeMongoQuery = ({ data, order, filter, select }) => {
+
+    filter = filter ? filter : {}
+    select = select ? select : {}
+
+    return new Promise(async (resolve, reject) => {
+        MongoClient.connect(mongoUri, { useUnifiedTopology: true }, (err, client) => {
+            const DB = client.db(mongoDatabase).collection(mongoCollection);
+
+            if (order && !err) {
+
+                switch (order) {
+                    case "Find":
+                        console.log(filter, { user: 0, _id: 0 })
+                        DB.find(filter, { user: 0, _id: 0 }).toArray((err, docs) => {
+                            client.close();
+                            if (err) reject({
+                                ReturnMessage: err.toString(),
+                                ReturnCode: -10,
+                                ReturnType: 3,
+                                docs
+                            });
+                            resolve(docs);
+                        });
+                        break;
+                    case "InsertOne":
+                        DB.insertOne(data, (err, response) => {
+                            client.close();
+                            if (err) reject(err)
+                            resolve(response)
+                        })
+                        break;
+                    case "InsertMany":
+                        if (!Array.isArray(data) || !data.length > 0) {
+                            
+                            DB.insertMany(data, function (err, docs) {
+                                assert.equal(err, null);
+                                client.close();
+                                resolve(docs)
+                            });
+
+                        }
+                        reject('Data can not inserted as Many')
+
+                        break;
+                    default:
+                        reject('Cannot find the order');
+                        break;
+                }
+            } else {
+                client.close();
+                reject('Cannot run the query');
+            }
+        });
+    });
+};
+
+
 module.exports = {
     IsValidJSONString,
-    StringFilter
+    StringFilter,
+    executeMongoQuery
 }
--- app-persisted/index.js
+++ app-persisted/index.js
@@ -3,45 +3,25 @@ const util = require('util');
 const FR = util.promisify(fs.readdir);
 const FS = util.promisify(fs.readFile);
 const print = console.log;
-const dir = './storage_JSON/'
-const bigData = new Array
+
+
+const express = require('express')
+const app = express()
+
+
+// const { executeMongoQuery } = require('./helper')
+
+// app.get('/', async function (req, res) {
+
+//     const doc = await executeMongoQuery({ order: 'Find', filter: { "_id": 1247805138707701800 } })
+
+
+//     res.json(doc)
+// })
 
 
 
+// app.listen(process.env.APP_PORT, () => console.log(`Nodejs App is running on port : ${process.env.APP_PORT}`))
 
 
 
-const { MongoClient } = require('mongodb');
-
-const url = 'mongodb://localhost:27017';
-
-
-const dbName = 'bisaai';
-
-
-MongoClient.connect(url, function (err, client) {
-
-    console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    client.close();
-});
-
-
-
-
-
-
-
-// FR(dir).then(
-//     snap => {
-//         snap.map(async el => {
-//             let datum = await FS(dir + el, 'utf8')
-//             datum = JSON.parse(datum)
-//             datum['_id'] = datum['id']
-//             delete datum.id
-//             return console.log(datum)
-//         })
-//     }
-// ) 
