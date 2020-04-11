1f9872ce65cc840b3f41f37472c0812bdde88b74
abdurhasan <abdurhasan20@gmail.com>
2020-04-11 18:31:38 +0700

    remake .env

--- app-webservice/src/routes/index.js
+++ app-webservice/src/routes/index.js
@@ -6,32 +6,22 @@ module.exports = function (app, dbs) {
     const Query = mquery(dbs)
     const defaultSelect = { user: 1, timestamp_ms: 1, label: 1, text: 1 }
 
-
-    //   :/
-    //   ?detail=true&id=1247804692106641400
     app.get('/', async (req, res) => {
         const { detail, id, select } = req.query
+
         let selectOption = select ? arrayKeyObject(select.split(','), 1) : defaultSelect
         if (detail == 'true') selectOption = {}
-
-        const rows = await Query.select(selectOption).find({})
+        
+        print(selectOption)
+        let rows = await Query.select(selectOption).find({})
+        
 
         res.json(rows)
 
 
     })
+
     return app
 }
 
 
-// rows = rows.map(rw => {
-
-//     return {
-//         id: rw._id,
-//         label: rw.label,
-//         tweet: rw.text,
-//         followers: rw.user.followers_count
-
-//     }
-
-// })
