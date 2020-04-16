63ef8354d6b58fcc6db041c213c33b2bdafc1295
muhammadiqbal.adz <abdurhasan20@gmail.com>
2020-04-16 20:00:30 +0700

    update .env.example

--- app-persisted/src/db/index.js
+++ app-persisted/src/db/index.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require('mongodb')
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017'
-const mongoDatabase = process.env.MONGODB_DATABASE || 'bisaai'
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bisaai'
 const mongoCollection = process.env.MONGODB_COLLECTION || 'twitter'
 require('dotenv').config()
 
@@ -9,7 +8,7 @@ require('dotenv').config()
 
 module.exports = function () {
     return MongoClient.connect(mongoURI, { useUnifiedTopology: true })
-        .then(client => Promise.resolve(client.db(mongoDatabase).collection(mongoCollection)))
+        .then(client => Promise.resolve(client.db().collection(mongoCollection)))
         .catch(err => Promise.reject(err))
 
 }
